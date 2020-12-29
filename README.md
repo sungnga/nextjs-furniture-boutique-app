a2d94d7701df6fa0f161675a8a17487394908fe9
sungnga <nga422@gmail.com>
2020-12-28 18:28:45 -0800

    Get route data from useRouter, create active links

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NOTES ON BUILDING THIS WEB APP
+# MY NOTES WHEN BUILDING THIS WEB APP
 
 ### PROJECT SETUP
 - Download starter project directory from https://github.com/reedbarger/react-reserve
@@ -21,3 +21,48 @@
   - If user not logged in, show the Login and Signup links and hide the Create link
   - If user is logged in, show the Account link and Logout button 
   - Use Semantic UI to style the Header component
+
+**2. Get Route Data From useRouter Hook, Create Active Links**
+- Since we're using a function component, Next provides a React useRouter() hook that gives us information from our router
+- `import { useRouter } from 'next/router'`
+- When execute, it will return a router object: `const router = useRouter()`
+- We can get information about what path we're on from the router object by using the property `router.pathname`
+- We can use this information to set the navbar menu item to be active when a user is on that particular route
+- In Header.js file:
+  - Write an isActive function that checks whether the given route matches with `router.pathname`. Return true or false
+  - If it is matched, apply the `active` property to that navbar menu item
+  ```js
+  import { useRouter } from 'next/router';
+
+  function Header() {
+    // When execute, userRouter hook returns a router object
+    const router = useRouter();
+    const user = false;
+
+    // Check if the given route matches with router.pathname
+    // router.pathname gives information about the route the user is on
+    function isActive(route) {
+      // return true or false
+      return route === router.pathname;
+    }
+
+    // Apply active style to menu item if the function returns true
+    <Menu.Item header active={isActive('/')}>
+  }
+  ```
+
+
+
+
+
+
+
+
+
+
+
+
+
+## RESOURCES
+- Next.js docs: https://nextjs.org/docs/getting-started
+- Semantic UI docs: https://react.semantic-ui.com/
--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,15 +1,25 @@
 import { Fragment } from 'react';
 import { Menu, Container, Image, Icon } from 'semantic-ui-react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 function Header() {
-	const user = true;
+	// When execute, it returns a router object
+	const router = useRouter();
+	const user = false;
+
+	// Check if the given route matches with router.pathname
+	// router.pathname gives information about the route the user is on
+	function isActive(route) {
+		// return true or false
+		return route === router.pathname;
+	}
 
 	return (
 		<Menu fluid inverted id='menu'>
 			<Container text>
 				<Link href='/'>
-					<Menu.Item header>
+					<Menu.Item header active={isActive('/')}>
 						<Image
 							size='mini'
 							src='/static/logo.svg'
@@ -20,7 +30,7 @@ function Header() {
 				</Link>
 
 				<Link href='/cart'>
-					<Menu.Item header>
+					<Menu.Item header active={isActive('/cart')}>
 						<Icon name='cart' size='large' />
 						Cart
 					</Menu.Item>
@@ -28,7 +38,7 @@ function Header() {
 
 				{user && (
 					<Link href='/create'>
-						<Menu.Item header>
+						<Menu.Item header active={isActive('/create')}>
 							<Icon name='add square' size='large' />
 							Create
 						</Menu.Item>
@@ -38,7 +48,7 @@ function Header() {
 				{user ? (
 					<Fragment>
 						<Link href='/account'>
-							<Menu.Item header>
+							<Menu.Item header active={isActive('/account')}>
 								<Icon name='user' size='large' />
 								Account
 							</Menu.Item>
@@ -52,14 +62,14 @@ function Header() {
 				) : (
 					<Fragment>
 						<Link href='/login'>
-							<Menu.Item header>
+							<Menu.Item header active={isActive('/login')}>
 								<Icon name='sign in' size='large' />
 								Login
 							</Menu.Item>
 						</Link>
 
 						<Link href='/signup'>
-							<Menu.Item header>
+							<Menu.Item header active={isActive('/signup')}>
 								<Icon name='signup' size='large' />
 								Signup
 							</Menu.Item>
