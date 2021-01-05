09e9ab8fe2cd457604f2d0ec5a08bf5de2d707cf
sungnga <nga422@gmail.com>
2021-01-05 09:53:04 -0800

    Initial commit

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
 	process.env.NODE_ENV === 'production'
-		? 'https://furnitureboutique.vercel.app'
+		? 'https://furnitureboutique.herokuapp.com'
 		: 'http://localhost:3000';
 
 export default baseUrl;
